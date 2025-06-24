386af874a9a9ec8175f896a8faaeb22bb6836c4d
marcus aurelio <marcusaurelio743@gmail.com>
2025-06-23 21:33:15 -0300

    revisando alguns componentes e anotações em jsf

---
+++ meuprimeiroprojetojsf/src/main/java/br/com/jsf/PessoaBean.java
@@ -0,0 +1,41 @@
+package br.com.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+
+	private String nome;
+	private String sobrenome;
+	private String nomeCompleto;
+	
+	public String MostrarNome() {
+		nomeCompleto = nome+" || "+ sobrenome;
+		
+		return "";
+	}
+	
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+}
